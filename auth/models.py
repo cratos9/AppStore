2ae06cd1eaace5c281d1c0dba061ed01705d6bbd
Miguel Angel Quiroz Hernandez <mq583680@gmail.com>
2024-09-13 15:33:33 -0600

    creacion tabla usuarios

--- auth/models.py
+++ auth/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    id = models.IntegerField(null=False, unique=True, primary_key=True, editable=False,db_index=True )
+    user = models.CharField(null=False, editable=True, max_length=50)
+    email = models.EmailField(null=False, editable=True, unique=True)
+    password = models.CharField(null=False, editable=True)
+    
+    def _repr_(self):
+        return f'<User: {self.username}>'
