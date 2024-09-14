76289ad775d03c090a45a24f83f2bb8d71cc130c
Miguel Angel Quiroz Hernandez <mq583680@gmail.com>
2024-09-14 14:17:20 -0600

    solucion de problema y creacion de nueva tabla en la base de datos

--- app_auth/apps.py
+++ app_auth/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class AuthConfig(AppConfig):
+class App_authConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'auth'
+    name = 'app_auth'
--- app_auth/models.py
+++ app_auth/models.py
@@ -4,7 +4,7 @@ class User(models.Model):
     id = models.IntegerField(null=False, unique=True, primary_key=True, editable=False,db_index=True )
     user = models.CharField(null=False, editable=True, max_length=50)
     email = models.EmailField(null=False, editable=True, unique=True)
-    password = models.CharField(null=False, editable=True)
+    password = models.CharField(null=False, editable=True, max_length=20)
     
     def _repr_(self):
         return f'<User: {self.username}>'
