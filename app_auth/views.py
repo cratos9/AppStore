e10784257ecd32264e764c5bc3d0e2bdfbf8e537
Miguel Angel Quiroz Hernandez <mq583680@gmail.com>
2024-09-17 11:15:19 -0600

    funcionalidad login

--- app_auth/views.py
+++ app_auth/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib.auth.hashers import make_password, check_password
 from .form import loginForm, RegisterForm
 from .models import User
 
@@ -9,8 +10,16 @@ def login(request):
     if request.method == 'POST':
         form = loginForm(request.POST)
         if form.is_valid():
-            user = form.cleaned_data['user']
+            email = form.cleaned_data['email']
             password = form.cleaned_data['password']
+            try:
+                user = User.objects.get(email = email)
+                if check_password(password, user.password):
+                    print("acceso permitido")
+                else:
+                    print("contrasena incorrecta")
+            except:
+                print("el usuario no existe")
     else:
         form = loginForm()
     return render(request,"auth/login.html",{'form':form})
@@ -22,7 +31,8 @@ def Register(request):
             user = form.cleaned_data['user']
             email = form.cleaned_data['email']
             password = form.cleaned_data['password']
-            newUser = User(user = user, email = email, password = password)
+            passwordSave = make_password(password)
+            newUser = User(user = user, email = email, password = passwordSave)
             newUser.save()
     else:
         form = RegisterForm()
