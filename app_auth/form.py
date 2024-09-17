41c48ae35961b370e7020263effe4bddc4ed5418
Miguel Angel Quiroz Hernandez <mq583680@gmail.com>
2024-09-16 21:18:19 -0600

    correccion y completo registro

--- app_auth/form.py
+++ app_auth/form.py
@@ -2,9 +2,9 @@ from django import forms
 
 class loginForm(forms.Form):
     user = forms.CharField(max_length= 50, label= "Usuario:", widget=forms.TextInput(attrs={'Class':'form--campos'}),required = True)
-    email = forms.EmailField(label="Correo electronico:", widget=forms.EmailInput(attrs={'Class':'form--campos'}), required=True)
     password = forms.CharField(label="Contrasena:", widget=forms.PasswordInput(attrs={'Class':'form--campos'}), required=True)
     
 class RegisterForm(forms.Form):
     user = forms.CharField(max_length= 50, label= "Usuario:", widget=forms.TextInput(attrs={'Class':'form--campos'}),required = True)
+    email = forms.EmailField(label="Correo electronico:", widget=forms.EmailInput(attrs={'Class':'form--campos'}), required=True)
     password = forms.CharField(label="Contrasena:", widget=forms.PasswordInput(attrs={'Class':'form--campos'}), required=True)
--- app_auth/views.py
+++ app_auth/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .form import loginForm, RegisterForm
+from .models import User
 
 def index(request):
     return render(request,"auth/index.html")
@@ -9,7 +10,6 @@ def login(request):
         form = loginForm(request.POST)
         if form.is_valid():
             user = form.cleaned_data['user']
-            email = form.cleaned_data['email']
             password = form.cleaned_data['password']
     else:
         form = loginForm()
@@ -20,7 +20,10 @@ def Register(request):
         form = RegisterForm(request.POST)
         if form.is_valid():
             user = form.cleaned_data['user']
+            email = form.cleaned_data['email']
             password = form.cleaned_data['password']
+            newUser = User(user = user, email = email, password = password)
+            newUser.save()
     else:
         form = RegisterForm()
     return render(request,"auth/register.html",{'form':form})
