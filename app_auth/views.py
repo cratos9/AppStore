ecc02f2307cc99a0bb74bdca2f95facb93019e20
Miguel Angel Quiroz Hernandez <mq583680@gmail.com>
2024-09-22 16:59:05 -0600

    mensajes de error en register

--- app_auth/views.py
+++ app_auth/views.py
@@ -1,11 +1,16 @@
-from django.shortcuts import render
+#Django imports
+from django.shortcuts import render, redirect
 from django.contrib.auth.hashers import make_password, check_password
+from django.contrib import messages
+#Personal imports
 from .form import loginForm, RegisterForm
 from .models import User
 
+#index route
 def index(request):
     return render(request,"auth/index.html")
 
+#Login logic and route
 def login(request):
     if request.method == 'POST':
         form = loginForm(request.POST)
@@ -24,6 +29,7 @@ def login(request):
         form = loginForm()
     return render(request,"auth/login.html",{'form':form})
 
+#register logic and route
 def Register(request):
     if request.method == 'POST':
         form = RegisterForm(request.POST)
@@ -31,9 +37,18 @@ def Register(request):
             user = form.cleaned_data['user']
             email = form.cleaned_data['email']
             password = form.cleaned_data['password']
-            passwordSave = make_password(password)
-            newUser = User(user = user, email = email, password = passwordSave)
-            newUser.save()
+            passwordConfirm = form.cleaned_data['passwordConfirm']
+            userExist = User.objects.filter(email=email).exists()
+            if userExist == False:
+                if password != passwordConfirm:
+                    messages.error(request, "La contrasena debe ser igual")
+                elif password == passwordConfirm:
+                    passwordSave = make_password(password)
+                    newUser = User(user = user, email = email, password = passwordSave)
+                    newUser.save()
+                    return redirect("login")
+            else:
+                messages.error(request, "El usuario ya existe")
     else:
         form = RegisterForm()
     return render(request,"auth/register.html",{'form':form})
