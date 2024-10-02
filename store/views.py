e048f1b1de2db60dc761776262a857cc872b7712
Miguel Angel Quiroz Hernandez <mq583680@gmail.com>
2024-10-01 21:01:03 -0600

    se empezo la pagina de vender

--- store/views.py
+++ store/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render, redirect
 
 def profile(request):
     return render(request,"store/profile.html")
+
+def sell(request):
+    return render(request,"store/sell.html")
